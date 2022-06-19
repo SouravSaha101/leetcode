0115e405760e8705426ecf1077931f82617012f4
sourav saha <souravsaha@rippling.com>
2022-06-19 20:55:17 +0530

    house robber

--- DP/Med/houseRobber.js
+++ DP/Med/houseRobber.js
@@ -1,1 +1,14 @@
+// DP
 // https://leetcode.com/problems/house-robber/
+
+var rob = function (nums) {
+  let rob1 = 0;
+  let rob2 = 0;
+  // [rob1, rob2, i , i+1, i+2]
+  for (let i of nums) {
+    let temp = Math.max(i + rob1, rob2);
+    rob1 = rob2;
+    rob2 = temp;
+  }
+  return rob2;
+};
